2c918df4d4c611e3b0f72b893dfa827c61eecc9d
ceyre-boop <ceyre@mcc.edu>
2026-03-15 16:01:03 -0400

    Update creator data - 3/15/2026

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 // Taboost Agency - Complete Creator Data
-// Generated: 2026-03-15T18:42:52.919Z
+// Generated: 2026-03-15T20:00:57.611Z
 // Total: 814 creators
 
 const creatorsData = [
@@ -32567,7 +32567,7 @@ const creatorsData = [
 
 const taboostData = {
   creators: creatorsData,
-  lastUpdated: "2026-03-15T18:42:52.919Z",
+  lastUpdated: "2026-03-15T20:00:57.611Z",
   getAllCreators: function() { return this.creators; },
   getCreator: function(username) { return this.creators.find(c => c.username === username.toLowerCase()); },
   loadFromCSV: async function() { return this.creators; }
