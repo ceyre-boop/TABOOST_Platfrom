4ffa98a9d9337fc2f870ae0cb1e67c11c54161b5
colin eyre <ceyre@mcc.edu>
2026-03-18 00:39:54 -0400

    Add estRev mapping in loadFromCSV to pull from rewardsMonth

--- js/sheets-data.js
+++ js/sheets-data.js
@@ -273,6 +273,7 @@ class TaboostDataService {
                 gifted: c.gifted || 0,
                 running: c.running || 0,
                 unlocked: c.unlocked || 0,
+                estRev: c.rewardsMonth || c.estRev || 0, // Dollar value from Rewards Month
                 rewardsMonth: c.rewardsMonth || '', // Column AO
                 rewards: {
                     earned: c.earned || 0,
