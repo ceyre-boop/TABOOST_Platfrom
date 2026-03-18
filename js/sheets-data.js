15d2fea7aba3c2e036114e55a6caed2b7daba92c
colin eyre <ceyre@mcc.edu>
2026-03-17 22:52:07 -0400

    Fix column indices for Days Month (39) and Hours Month (40) - correct positions based on column order

--- js/sheets-data.js
+++ js/sheets-data.js
@@ -174,8 +174,8 @@ class TaboostDataService {
             diamondsTwoMonthsAgo: this.formatNumber(getValue('-2 Month 💎')),
             
             // Goals data - Column AP (Days Month), Column AQ (Hours Month)
-            daysGoal: parseInt(values[38]) || parseInt(getValue('Days Month')) || 22, // Column AP = index 38 (Days Month)
-            hoursGoal: parseInt(values[39]) || parseInt(getValue('Hours Month')) || 60, // Column AQ = index 39 (Hours Month)
+            daysGoal: parseInt(values[39]) || parseInt(getValue('Days Month')) || 22, // Column AP = index 39 (Days Month)
+            hoursGoal: parseInt(values[40]) || parseInt(getValue('Hours Month')) || 80, // Column AQ = index 40 (Hours Month)
             
             // Column AO - Rewards Month (March 2026 onwards)
             rewardsMonth: getValue('Rewards Month') || values[40] || '', // Column AO
