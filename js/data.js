ed3b7628ea2288c60d28726e4b3005076c42ace5
GitHub Action <action@github.com>
2026-07-12 23:51:13 +0000

    Auto-update creator data from CSV

--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 // Taboost Agency - Complete Creator Data
-// Generated: 2026-07-12T20:29:11.313Z
+// Generated: 2026-07-12T23:51:13.637Z
 // Total: 974 creators
 
 const creatorsData = [
@@ -38967,7 +38967,7 @@ const creatorsData = [
 
 const taboostData = {
   creators: creatorsData,
-  lastUpdated: "2026-07-12T20:29:11.313Z",
+  lastUpdated: "2026-07-12T23:51:13.637Z",
   getAllCreators: function() { return this.creators; },
   getCreator: function(username) { return this.creators.find(c => c.username === username.toLowerCase()); },
   loadFromCSV: async function() { return this.creators; }
